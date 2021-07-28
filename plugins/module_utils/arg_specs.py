8697f4aa82bc54024bd08bf8aea8fe737738bc99
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2021-07-27 22:13:43 -0400

    switch to first generation generated code for requried arg specs

--- plugins/module_utils/arg_specs.py
+++ plugins/module_utils/arg_specs.py
@@ -4,26 +4,48 @@ __metaclass__ = type
 # module argument specs derived from TrueNAS API v2.0 OpenAPI Spec 3.0 definition
 
 # TODO: define specs as dict for resource module implementations to refer to by key name ?
-# TODO: generate specs from OAS ?
 
 mail_update_arg_spec = {
     'type': 'dict',
     'options': {
-        'fromemail': dict(type='str', required=True),
-        'fromname': dict(type='str'),
-        'outgoingserver': dict(type='str'),
-        'port': dict(type='int', required=True),
-        'security': dict(type='str', default='PLAIN', choices=['PLAIN', 'SSL', 'TLS']),
-        'smtp': dict(type='bool', aliases=['smtp_auth']),
-        'user': dict(type='str'),
-        'pass': dict(type='str'),
-        'oauth': dict(
-            type='dict',
-            options=dict(
-                client_id=dict(type='str'),
-                client_secret=dict(type='str'),
-                refresh_token=dict(type='str'),
-            )
-        )
+        'fromemail': {
+            'type': 'str',
+        },
+        'fromname': {
+            'type': 'str',
+        },
+        'outgoingserver': {
+            'type': 'str',
+        },
+        'port': {
+            'type': 'int',
+        },
+        'security': {
+            'type': 'str',
+            'choices': ['PLAIN', 'SSL', 'TLS'],
+        },
+        'smtp': {
+            'type': 'bool',
+        },
+        'user': {
+            'type': 'str',
+        },
+        'pass': {
+            'type': 'str',
+        },
+        'oauth': {
+            'type': 'dict',
+            'options': {
+                'client_id': {
+                    'type': 'str',
+                },
+                'client_secret': {
+                    'type': 'str',
+                },
+                'refresh_token': {
+                    'type': 'str',
+                },
+            }
+        },
     }
 }
